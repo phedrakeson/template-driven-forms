53aa4a058e516a2148b42c472b86218be4577b01
Gabriel Duarte <gabrield.developer@gmail.com>
2021-05-03 17:53:56 -0300

    feat: aplica css na validacao dos controles

--- src/app/features/form/form.component.ts
+++ src/app/features/form/form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Usuario',
-    email: 'usuario@email.com'
+    nome: null,
+    email: null
   }
 
   constructor() { }
